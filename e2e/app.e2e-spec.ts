4d7ad0c30f679fce2ed9fe9c5dde4240a82b7a7c
Rivka Strauss <rvkstrauss@gmail.com>
2017-08-25 13:24:32 +0300

    initial skeleton

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularTestProjectPage } from './app.po';
+
+describe('angular-test-project App', () => {
+  let page: AngularTestProjectPage;
+
+  beforeEach(() => {
+    page = new AngularTestProjectPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
